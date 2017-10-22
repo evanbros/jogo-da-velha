6059c58e1139a48e1901c76c09e61ed3468d8052
Tasso Evangelista <tasso@tassoevan.me>
2017-10-22 21:36:58 -0200

    Botões em array

--- javascript.js
+++ javascript.js
@@ -5,30 +5,23 @@ const tabuleiro = [
   [ ' ', ' ', ' ' ],
   [ ' ', ' ', ' ' ],
 ];
-
-var jogar1 = document.getElementById('jogar1');
-var jogar2 = document.getElementById('jogar2');
-var jogar3 = document.getElementById('jogar3');
-var jogar4 = document.getElementById('jogar4');
-var jogar5 = document.getElementById('jogar5');
-var jogar6 = document.getElementById('jogar6');
-var jogar7 = document.getElementById('jogar7');
-var jogar8 = document.getElementById('jogar8');
-var jogar9 = document.getElementById('jogar9');
-
 let ultimaJogada;
+
+const botoes = document.querySelectorAll(
+  '#jogar1, #jogar2, #jogar3, #jogar4, #jogar5, #jogar6, #jogar7, #jogar8, #jogar9'
+);
 
 const exibirTabuleiro = () => {
     let saida = tabuleiro.map(
     linha => linha.join('|')
     ).join('\n-+-+-\n');
-  
+
     let vencedor = calcularVencedor();
-  
+
     if (vencedor !== 'nenhum') {
         saida += '\nVencedor: ' + vencedor;
     }
-  
+
     document.getElementById('tabuleiro').innerHTML = saida.replace(/ /g, '&nbsp;').replace(/\n/g, '<br />');
 }
 
@@ -48,66 +41,66 @@ const calcularVencedor = () => {
     // Linha cheia
     for (let i = 0; i <= 2; ++i) {
         if (tabuleiro[i][0] === tabuleiro[i][1] && tabuleiro[i][1] === tabuleiro[i][2] && tabuleiro[i][0] !== ' ') {
-            jogar1.disabled = true;
-            jogar2.disabled = true;
-            jogar3.disabled = true;
-            jogar4.disabled = true;
-            jogar5.disabled = true;
-            jogar6.disabled = true;
-            jogar7.disabled = true;
-            jogar8.disabled = true;
-            jogar9.disabled = true;
+            botoes[0].disabled = true;
+            botoes[1].disabled = true;
+            botoes[2].disabled = true;
+            botoes[3].disabled = true;
+            botoes[4].disabled = true;
+            botoes[5].disabled = true;
+            botoes[6].disabled = true;
+            botoes[7].disabled = true;
+            botoes[8].disabled = true;
             return tabuleiro[i][0];
         }
     }
-  
+
     // Coluna cheia
     for (let j = 0; j <= 2; ++j) {
         if (tabuleiro[0][j] === tabuleiro[1][j] && tabuleiro[1][j] === tabuleiro[2][j] && tabuleiro[0][j] !== ' ') {
-            jogar1.disabled = true;
-            jogar2.disabled = true;
-            jogar3.disabled = true;
-            jogar4.disabled = true;
-            jogar5.disabled = true;
-            jogar6.disabled = true;
-            jogar7.disabled = true;
-            jogar8.disabled = true;
-            jogar9.disabled = true;
+            botoes[0].disabled = true;
+            botoes[1].disabled = true;
+            botoes[2].disabled = true;
+            botoes[3].disabled = true;
+            botoes[4].disabled = true;
+            botoes[5].disabled = true;
+            botoes[6].disabled = true;
+            botoes[7].disabled = true;
+            botoes[8].disabled = true;
             return tabuleiro[0][j];
-        }   
+        }
     }
-  
+
     // Diagonal principal cheia
     if (tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2] && tabuleiro[0][0] !== ' ') {
-        jogar1.disabled = true;
-        jogar2.disabled = true;
-        jogar3.disabled = true;
-        jogar4.disabled = true;
-        jogar5.disabled = true;
-        jogar6.disabled = true;
-        jogar7.disabled = true;
-        jogar8.disabled = true;
-        jogar9.disabled = true;
+        botoes[0].disabled = true;
+        botoes[1].disabled = true;
+        botoes[2].disabled = true;
+        botoes[3].disabled = true;
+        botoes[4].disabled = true;
+        botoes[5].disabled = true;
+        botoes[6].disabled = true;
+        botoes[7].disabled = true;
+        botoes[8].disabled = true;
         return tabuleiro[0][0];
     }
-  
+
     // Diagonal inversa cheia
     if (tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0] && tabuleiro[0][2] !== ' ') {
-        jogar1.disabled = true;
-        jogar2.disabled = true;
-        jogar3.disabled = true;
-        jogar4.disabled = true;
-        jogar5.disabled = true;
-        jogar6.disabled = true;
-        jogar7.disabled = true;
-        jogar8.disabled = true;
-        jogar9.disabled = true;
+        botoes[0].disabled = true;
+        botoes[1].disabled = true;
+        botoes[2].disabled = true;
+        botoes[3].disabled = true;
+        botoes[4].disabled = true;
+        botoes[5].disabled = true;
+        botoes[6].disabled = true;
+        botoes[7].disabled = true;
+        botoes[8].disabled = true;
         return tabuleiro[0][2];
     }
-  
+
     // Empate
     let preenchidos = 0;
-  
+
     for (let i = 0; i <= 2; ++i) {
         for (let j = 0; j <= 2; ++j) {
             if (tabuleiro[i][j] !== ' ') {
@@ -115,18 +108,18 @@ const calcularVencedor = () => {
             }
         }
     }
-  
+
     if (preenchidos === 9) {
         return 'empate';
     }
-   
+
     return 'nenhum';
-  
+
 };
 
 let i, j;
 
-jogar1.addEventListener('click', function () {
+botoes[0].addEventListener('click', function () {
     i = 0;
     j = 0;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -134,7 +127,7 @@ jogar1.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar2.addEventListener('click', function () {
+botoes[1].addEventListener('click', function () {
     i = 0;
     j = 1;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -142,7 +135,7 @@ jogar2.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar3.addEventListener('click', function () {
+botoes[2].addEventListener('click', function () {
     i = 0;
     j = 2;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -150,7 +143,7 @@ jogar3.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar4.addEventListener('click', function () {
+botoes[3].addEventListener('click', function () {
     i = 1;
     j = 0;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -158,7 +151,7 @@ jogar4.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar5.addEventListener('click', function () {
+botoes[4].addEventListener('click', function () {
     i = 1;
     j = 1;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -166,7 +159,7 @@ jogar5.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar6.addEventListener('click', function () {
+botoes[5].addEventListener('click', function () {
     i = 1;
     j = 2;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -174,7 +167,7 @@ jogar6.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar7.addEventListener('click', function () {
+botoes[6].addEventListener('click', function () {
     i = 2;
     j = 0;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -182,7 +175,7 @@ jogar7.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar8.addEventListener('click', function () {
+botoes[7].addEventListener('click', function () {
     i = 2;
     j = 1;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -190,7 +183,7 @@ jogar8.addEventListener('click', function () {
     this.disabled = true;
 }, false);
 
-jogar9.addEventListener('click', function () {
+botoes[8].addEventListener('click', function () {
     i = 2;
     j = 2;
     marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
@@ -205,15 +198,15 @@ const nj = document.getElementById('nj');
 nj.addEventListener('click', function (){
     limparTabuleiro();
     exibirTabuleiro();
-    jogar1.disabled = false;
-    jogar2.disabled = false;
-    jogar3.disabled = false;
-    jogar4.disabled = false;
-    jogar5.disabled = false;
-    jogar6.disabled = false;
-    jogar7.disabled = false;
-    jogar8.disabled = false;
-    jogar9.disabled = false;
+    botoes[0].disabled = false;
+    botoes[1].disabled = false;
+    botoes[2].disabled = false;
+    botoes[3].disabled = false;
+    botoes[4].disabled = false;
+    botoes[5].disabled = false;
+    botoes[6].disabled = false;
+    botoes[7].disabled = false;
+    botoes[8].disabled = false;
 },false);
 
 }
