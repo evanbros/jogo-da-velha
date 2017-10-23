7372f9b5e5e6c896a2f4392705f3c987784774ac
Tasso Evangelista <tasso@tassoevan.me>
2017-10-22 22:35:55 -0200

    Arquitetura MVC

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+// Estado da Aplicação
+// Aqui vão todas as variáveis que serão modificadas durante o andamento do jogo
 
 const tabuleiro = [
   [ ' ', ' ', ' ' ],
@@ -7,124 +8,129 @@ const tabuleiro = [
 ];
 let ultimaJogada;
 
-const botoes = document.querySelectorAll(
-  '#jogar1, #jogar2, #jogar3, #jogar4, #jogar5, #jogar6, #jogar7, #jogar8, #jogar9'
-);
-
-const exibirTabuleiro = () => {
-    let saida = tabuleiro.map(
-    linha => linha.join('|')
-    ).join('\n-+-+-\n');
-
-    let vencedor = calcularVencedor();
-
-    if (vencedor !== 'nenhum') {
-        saida += '\nVencedor: ' + vencedor;
-    }
-
-    document.getElementById('tabuleiro').innerHTML = saida.replace(/ /g, '&nbsp;').replace(/\n/g, '<br />');
-}
-
-const atualizarBotoes = () => {
-  const vencedor = calcularVencedor();
-  if (vencedor !== 'nenhum') {
-    desabilitarBotoes();
-  }
-};
+// Lógica da Aplicação
+// Aqui vão todas as rotinas que modificam o estado da aplicação SEM efeitos colaterais (mudanças visuais, por exemplo)
 
 const limparTabuleiro = () => {
-    tabuleiro[0] = [ ' ', ' ', ' ' ];
-    tabuleiro[1] = [ ' ', ' ', ' ' ];
-    tabuleiro[2] = [ ' ', ' ', ' ' ];
+  tabuleiro[0] = [ ' ', ' ', ' ' ];
+  tabuleiro[1] = [ ' ', ' ', ' ' ];
+  tabuleiro[2] = [ ' ', ' ', ' ' ];
 };
 
 const marcar = (i, j, char) => {
-    tabuleiro[i][j] = char;
-    ultimaJogada = char;
-    exibirTabuleiro();
+  tabuleiro[i][j] = char;
+  ultimaJogada = char;
 };
 
 const calcularVencedor = () => {
-    // Linha cheia
-    for (let i = 0; i <= 2; ++i) {
-        if (tabuleiro[i][0] === tabuleiro[i][1] && tabuleiro[i][1] === tabuleiro[i][2] && tabuleiro[i][0] !== ' ') {
-            return tabuleiro[i][0];
-        }
+  // Linha cheia
+  for (let i = 0; i <= 2; ++i) {
+    if (tabuleiro[i][0] === tabuleiro[i][1] && tabuleiro[i][1] === tabuleiro[i][2] && tabuleiro[i][0] !== ' ') {
+      return tabuleiro[i][0];
     }
+  }
 
-    // Coluna cheia
+  // Coluna cheia
+  for (let j = 0; j <= 2; ++j) {
+    if (tabuleiro[0][j] === tabuleiro[1][j] && tabuleiro[1][j] === tabuleiro[2][j] && tabuleiro[0][j] !== ' ') {
+      return tabuleiro[0][j];
+    }
+  }
+
+  // Diagonal principal cheia
+  if (tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2] && tabuleiro[0][0] !== ' ') {
+    return tabuleiro[0][0];
+  }
+
+  // Diagonal inversa cheia
+  if (tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0] && tabuleiro[0][2] !== ' ') {
+    return tabuleiro[0][2];
+  }
+
+  // Empate
+  let preenchidos = 0;
+
+  for (let i = 0; i <= 2; ++i) {
     for (let j = 0; j <= 2; ++j) {
-        if (tabuleiro[0][j] === tabuleiro[1][j] && tabuleiro[1][j] === tabuleiro[2][j] && tabuleiro[0][j] !== ' ') {
-            return tabuleiro[0][j];
-        }
+      if (tabuleiro[i][j] !== ' ') {
+        ++preenchidos;
+      }
     }
+  }
 
-    // Diagonal principal cheia
-    if (tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2] && tabuleiro[0][0] !== ' ') {
-        return tabuleiro[0][0];
-    }
+  if (preenchidos === 9) {
+    return 'empate';
+  }
 
-    // Diagonal inversa cheia
-    if (tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0] && tabuleiro[0][2] !== ' ') {
-        return tabuleiro[0][2];
-    }
-
-    // Empate
-    let preenchidos = 0;
-
-    for (let i = 0; i <= 2; ++i) {
-        for (let j = 0; j <= 2; ++j) {
-            if (tabuleiro[i][j] !== ' ') {
-                ++preenchidos;
-            }
-        }
-    }
-
-    if (preenchidos === 9) {
-        return 'empate';
-    }
-
-    return 'nenhum';
-
+  return 'nenhum';
 };
 
-const criarHandlerParaMarcar = (i, j) => function () {
-  marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
-  calcularVencedor();
-  atualizarBotoes();
-  this.disabled = true;
-};
+// Apresentação da Aplicação
+// Aqui vai tudo responsável por exibir a aplicação para o usuário, bem como reagir às ações dele
 
-const habilitarBotoes = () => {
-  botoes.forEach(botao => {
-    botao.disabled = false;
-  });
-};
+window.onload = function () {
+  const botoes = document.querySelectorAll(
+    '#jogar1, #jogar2, #jogar3, #jogar4, #jogar5, #jogar6, #jogar7, #jogar8, #jogar9'
+  );
 
-const desabilitarBotoes = () => {
-  botoes.forEach(botao => {
-    botao.disabled = true;
-  });
-};
+  const exibirTabuleiro = () => {
+      let saida = tabuleiro.map(
+      linha => linha.join('|')
+      ).join('\n-+-+-\n');
 
-botoes[0].addEventListener('click', criarHandlerParaMarcar(0, 0), false);
-botoes[1].addEventListener('click', criarHandlerParaMarcar(0, 1), false);
-botoes[2].addEventListener('click', criarHandlerParaMarcar(0, 2), false);
-botoes[3].addEventListener('click', criarHandlerParaMarcar(1, 0), false);
-botoes[4].addEventListener('click', criarHandlerParaMarcar(1, 1), false);
-botoes[5].addEventListener('click', criarHandlerParaMarcar(1, 2), false);
-botoes[6].addEventListener('click', criarHandlerParaMarcar(2, 0), false);
-botoes[7].addEventListener('click', criarHandlerParaMarcar(2, 1), false);
-botoes[8].addEventListener('click', criarHandlerParaMarcar(2, 2), false);
+      let vencedor = calcularVencedor();
 
-exibirTabuleiro();
+      if (vencedor !== 'nenhum') {
+          saida += '\nVencedor: ' + vencedor;
+      }
 
-const nj = document.getElementById('nj');
+      document.getElementById('tabuleiro').innerHTML = saida.replace(/ /g, '&nbsp;').replace(/\n/g, '<br />');
+  }
 
-nj.addEventListener('click', function (){
-    limparTabuleiro();
+  const atualizarBotoes = () => {
+    const vencedor = calcularVencedor();
+    if (vencedor !== 'nenhum') {
+      desabilitarBotoes();
+    }
+  };
+
+  const criarHandlerParaMarcar = (i, j) => function () {
+    marcar(i, j, ultimaJogada === 'x' ? 'o' : 'x');
     exibirTabuleiro();
-    habilitarBotoes();
-},false);
+    calcularVencedor();
+    atualizarBotoes();
+    this.disabled = true;
+  };
 
+  const habilitarBotoes = () => {
+    botoes.forEach(botao => {
+      botao.disabled = false;
+    });
+  };
+
+  const desabilitarBotoes = () => {
+    botoes.forEach(botao => {
+      botao.disabled = true;
+    });
+  };
+
+  botoes[0].addEventListener('click', criarHandlerParaMarcar(0, 0), false);
+  botoes[1].addEventListener('click', criarHandlerParaMarcar(0, 1), false);
+  botoes[2].addEventListener('click', criarHandlerParaMarcar(0, 2), false);
+  botoes[3].addEventListener('click', criarHandlerParaMarcar(1, 0), false);
+  botoes[4].addEventListener('click', criarHandlerParaMarcar(1, 1), false);
+  botoes[5].addEventListener('click', criarHandlerParaMarcar(1, 2), false);
+  botoes[6].addEventListener('click', criarHandlerParaMarcar(2, 0), false);
+  botoes[7].addEventListener('click', criarHandlerParaMarcar(2, 1), false);
+  botoes[8].addEventListener('click', criarHandlerParaMarcar(2, 2), false);
+
+  exibirTabuleiro();
+
+  const nj = document.getElementById('nj');
+
+  nj.addEventListener('click', function (){
+      limparTabuleiro();
+      exibirTabuleiro();
+      habilitarBotoes();
+  },false);
 }
